63f9487728bdd4be892cfe9b643d1cdbd2801eeb
Todor Tsankov <todor.v.tsankov@gmail.com>
2022-07-19 22:06:52 +0300

    Add SelectionSort.

---
+++ Playground/Algorithms/Sorting/SelectionSorter.cs
@@ -0,0 +1,28 @@
+﻿using Algorithms.Sorting.Contracts;
+
+namespace Algorithms.Sorting
+{
+    public class SelectionSorter : ISorter
+    {
+        public void Sort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var min = i;
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                var temp = arr[i];
+
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+        }
+    }
+}
