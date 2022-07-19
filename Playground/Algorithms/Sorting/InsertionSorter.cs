b117b9b3bc251b9c20ef58471a3f16d8cff9f5d3
Todor Tsankov <todor.v.tsankov@gmail.com>
2022-07-19 22:13:48 +0300

    Add InsertionSort.

---
+++ Playground/Algorithms/Sorting/InsertionSorter.cs
@@ -0,0 +1,28 @@
+﻿using Algorithms.Sorting.Contracts;
+
+namespace Algorithms.Sorting
+{
+    public class InsertionSorter : ISorter
+    {
+        public void Sort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int j = i; j >= 1; j--)
+                {
+                    if (arr[j] < arr[j - 1])
+                    {
+                        var temp = arr[j];
+
+                        arr[j] = arr[j - 1];
+                        arr[j - 1] = temp;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
